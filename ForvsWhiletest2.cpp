be6cf61380de4ca261a0018424b084295ec350f8
tsl2663 <tsl2663@users.noreply.github.com>
2016-03-30 01:30:42 -0400

    Update ForvsWhiletest2.cpp

--- ForvsWhiletest2.cpp
+++ ForvsWhiletest2.cpp
@@ -15,21 +15,20 @@ int main ()                 /** CPU program call */
 {
     int i = 0;              /** declaration and initialization of counter i */
 
-    cout << "This is the output from my while loop." << endl; /** information for user */
+    cout << "This is the output from my while loop." << endl;   /** information for user */
+    
     while ( i <= 13)        /** shows structure of a while loop */
         {
           cout << i << " x " << i << " = " << i*i << endl;
           i++;
         }
 
-
-    cout << "This is the output from my for loop." << endl; /** information for user */
+    cout << "This is the output from my for loop." << endl;   /** information for user */
+    
     for ( int i = 0; i <= 13; i++)  /** shows structure of a for loop */
         {
           cout << i << " x " << i << " = " << i*i << endl;
-
         }
-
 
  return 0;    /**ends program */
  }
