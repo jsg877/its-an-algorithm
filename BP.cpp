a42674ac64734e1481f0a4b83bb9f59044056609
tsl2663 <tsl2663@users.noreply.github.com>
2016-03-29 21:54:38 -0400

    Update BP.cpp
    Added comments and polished the formatting.

--- BP.cpp
+++ BP.cpp
@@ -1,24 +1,32 @@
-#include <iostream>
+/*********************************************************
+// Tonya Lamb
+// CSC 110 Dr. Looms
+// Exam 2 prep
+/*********************************************************/
+
+
+#include <iostream>   /**Header files for the libraries I am using */
 #include <cmath>
-using namespace std;
+using namespace std;  /** Allows access to where the libraries make the declarations**/
 
-int main()
+int main()            /** this is what the CPU calls to start the program    **/
 {
-float total;
-float thisBP;
+float total;          /** declares a float variable total that will be the sum of all the BP **/
+float thisBP;         /** declares a float variable thisBP for each individual BP entry **/
 
 
-total = 0;
-cout << "Enter a blood pressure (-1 to stop)";
+total = 0;                                              /** initializes total to zero **/
+cout << "Enter a blood pressure (-1 to stop)" << endl;  /** prints out instructions to user **/
 cin >> thisBP;
 
-while(thisBP != -1)
+while(thisBP != -1)                                     /** loop for entering multiple BP's. Sentinel is set as -1 **/
 {
-total = total + thisBP;
-cout << "Enter a blood pressure (-1 to stop)";
+total = total + thisBP;                                 /** running sum of BP's **/
+cout << "Enter a blood pressure (-1 to stop)" << endl;  /** prints out instructions to user **/
 cin >> thisBP;
 }
-cout << total << " is the total of BP";
-return 0;
+cout << total << " is the total of the BP's entered.";  /** prints out the final total once loop is exited **/
+return 0;           /** ends program **/
 }
+
 
